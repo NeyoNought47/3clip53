c0bf42eb1a5cd116b249de51ab92f924857a7f31
Leonardo Furukawa <72781782+NeyoNought47@users.noreply.github.com>
2021-02-28 14:25:59 +0800

    Update presentAllHiddenFiles.sh

--- presentAllHiddenFiles.sh
+++ presentAllHiddenFiles.sh
@@ -1,9 +1,1 @@
-echo " "
-
-echo "[Enter the administrator's passcode to execute this script: ]"
-
 defaults write com.apple.finder AppleShowAllFiles -bool true; killall Finder
-
-echo "===================================== Gotcha ==========================================="
-
-
